34b5152e00f09a5bedde76ce6118b5cd079f0173
hajParsa <152877374+hajparsaHG@users.noreply.github.com>
2025-08-02 11:50:52 +0330

    add frontend

--- test/settings.py
+++ test/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fw0#@c7gxa(#9*b99iq^o1e+x1g8)pn!qpdguqg3(s(_#1egi$
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'my_app',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'test.urls'
